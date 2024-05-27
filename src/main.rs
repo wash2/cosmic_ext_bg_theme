3ab9c5680843056632373513e8784062e6ca02ad
Ashley Wulber <ashley@system76.com>
2024-05-27 01:25:20 -0400

    config options & palette

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use cosmic_bg_config::state::State;
+use cosmic_config::cosmic_config_derive::CosmicConfigEntry;
 use cosmic_config::{ConfigGet, ConfigSet, CosmicConfigEntry};
 use cosmic_settings_daemon::{ConfigProxy, CosmicSettingsDaemonProxy};
 use cosmic_theme::{Theme, ThemeBuilder};
@@ -6,7 +7,8 @@ use futures::StreamExt;
 use image::GenericImageView;
 use kmeans_colors::{get_kmeans, Kmeans, Sort};
 use palette::color_difference::Wcag21RelativeContrast;
-use palette::{Clamp, FromColor, IntoColor, Lab, Lch, Srgb, SrgbLuma, Srgba};
+use palette::{Clamp, FromColor, IntoColor, Lab, Lch, Saturate, Srgb, SrgbLuma, Srgba};
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{fmt, EnvFilter};
@@ -30,7 +32,7 @@ async fn main() -> anyhow::Result<()> {
     let settings_proxy = connect_settings_daemon().await?;
     let config = State::state()?;
     let (path, name) = settings_proxy.watch_state(cosmic_bg_config::NAME, State::version()).await?;
-    let bg_state_proxy = ConfigProxy::builder(settings_proxy.connection())
+    let bg_state_proxy = ConfigProxy::builder(settings_proxy.as_ref().connection())
         .path(path)?
         .destination(name)?
         .build()
@@ -46,7 +48,7 @@ async fn main() -> anyhow::Result<()> {
         },
     };
 
-    let kmeans = match apply_state(&state, true, None) {
+    match apply_state(&state, true) {
         Ok(kmeans) => Some(kmeans),
         Err(err) => {
             tracing::error!("Failed to apply the state: {}", err);
@@ -54,12 +56,12 @@ async fn main() -> anyhow::Result<()> {
         },
     };
 
-    if let Err(err) = apply_state(&state, false, kmeans) {
+    if let Err(err) = apply_state(&state, false) {
         tracing::error!("Failed to apply the state: {}", err);
     }
     let mut changes = bg_state_proxy.receive_changed().await?;
 
-    let mut ownership_change = settings_proxy.receive_owner_changed().await?;
+    let mut ownership_change = settings_proxy.as_ref().receive_owner_changed().await?;
 
     loop {
         let c = tokio::select! {
@@ -88,17 +90,17 @@ async fn main() -> anyhow::Result<()> {
             tracing::error!("Failed to update the state: {}", err);
         }
 
-        let kmeans = match apply_state(&state, true, None) {
+        match apply_state(&state, true) {
             Ok(kmeans) => Some(kmeans),
             Err(err) => {
                 tracing::error!("Failed to apply the state: {}", err);
                 None
             },
         };
-        if let Err(err) = apply_state(&state, true, None) {
+        if let Err(err) = apply_state(&state, true) {
             tracing::error!("Failed to apply the state: {}", err);
         }
-        if let Err(err) = apply_state(&state, false, kmeans) {
+        if let Err(err) = apply_state(&state, false) {
             tracing::error!("Failed to apply the state: {}", err);
         }
     }
@@ -133,11 +135,7 @@ async fn connect_settings_daemon() -> anyhow::Result<CosmicSettingsDaemonProxy<'
     Err(anyhow::anyhow!("Failed to connect to the settings daemon"))
 }
 
-fn apply_state(
-    state: &State,
-    is_dark: bool,
-    prev: Option<(Vec<Lab>, Vec<Lab>)>,
-) -> anyhow::Result<(Vec<Lab>, Vec<Lab>)> {
+fn apply_state(state: &State, is_dark: bool) -> anyhow::Result<()> {
     let Some(w) = state.wallpapers.get(0) else {
         anyhow::bail!("No wallpapers found");
     };
@@ -145,46 +143,99 @@ fn apply_state(
         anyhow::bail!("No wallpaper path");
     };
 
+    let bg_config = cosmic_config::Config::new(ID, MyConfig::VERSION)
+        .map(|c| match MyConfig::get_entry(&c) {
+            Ok(entry) => entry,
+            Err((errs, entry)) => {
+                for err in errs {
+                    tracing::error!("Failed to get the config: {}", err);
+                }
+                entry
+            },
+        })
+        .unwrap_or_default();
+
     let p = format!("{}_{}", path.to_string_lossy().replace("/", "_"), is_dark);
     if use_saved_result(&p, is_dark).is_ok() {
-        return Ok((Vec::new(), Vec::new()));
+        return Ok(());
     }
 
-    let (mut centroids, mut res) = match prev {
-        Some((kmeans, res)) if !res.is_empty() => (kmeans, res),
-        _ => {
-            let img: Vec<Lab> = image::open(path)?
-                .pixels()
-                .map(|(_, _, p)| {
-                    let p = p.0;
-                    let rgb = Srgb::<u8>::new(p[0], p[1], p[2]);
-                    rgb.into_format().into_color()
-                })
-                .collect();
+    let kmeans_p = format!("{}_kmeans", p);
 
-            let mut results = Vec::new();
-            let seed = 42;
-            // TODO elbow method
-            let mut best_result = Kmeans::new();
-            for i in 0..2 {
-                let run_result = get_kmeans(8, 40, 10., false, &img, seed + i as u64);
-                if run_result.score < best_result.score {
-                    best_result = run_result;
+    let kmeans_config = cosmic_config::Config::new_state(ID, 1);
+
+    let mut res =
+        match kmeans_config.as_ref().ok().and_then(|c| c.get::<KmeanState>(&kmeans_p).ok()) {
+            Some(res) if !res.0.is_empty() => res.0,
+            _ => {
+                let img: Vec<Lab> = image::io::Reader::open(path)?
+                    .with_guessed_format()?
+                    .decode()?
+                    .pixels()
+                    .map(|(_, _, p)| {
+                        let p = p.0;
+                        let rgb = Srgb::<u8>::new(p[0], p[1], p[2]);
+                        rgb.into_format().into_color()
+                    })
+                    .collect();
+
+                let mut results = Vec::new();
+                let seed = 42;
+                // TODO elbow method
+                let mut best_result = Kmeans::new();
+                for i in 0..2 {
+                    let run_result = get_kmeans(8, 40, 10., false, &img, seed + i as u64);
+                    if run_result.score < best_result.score {
+                        best_result = run_result;
+                    }
                 }
-            }
-            results.push(best_result);
+                results.push(best_result);
 
-            let Some(kmeans) = results.into_iter().last() else {
-                anyhow::bail!("No kmeans result");
-            };
-            let centroids = kmeans.centroids.clone();
-            let mut res = Lab::sort_indexed_colors(&centroids, &kmeans.indices);
-            res.sort_unstable_by(|a, b| (b.percentage).total_cmp(&a.percentage));
-            (centroids, res.into_iter().map(|c| c.centroid).collect::<Vec<Lab>>())
-        },
-    };
+                let Some(kmeans) = results.into_iter().last() else {
+                    anyhow::bail!("No kmeans result");
+                };
+                let centroids = kmeans.centroids.clone();
+                let mut res = Lab::sort_indexed_colors(&centroids, &kmeans.indices);
+                res.sort_unstable_by(|a, b| (b.percentage).total_cmp(&a.percentage));
 
-    let res_ret = res.clone();
+                let mut res = res.into_iter().map(|c| c.centroid).collect::<Vec<Lab>>();
+                // move avoid colors to the end
+                let avoid = if is_dark { &bg_config.avoid_dark } else { &bg_config.avoid_light };
+                let mut avoid_colors = Vec::new();
+                res.retain(|c| {
+                    if avoid.iter().any(|a| *a == (*c).into_color()) {
+                        avoid_colors.push(c.clone());
+                        false
+                    } else {
+                        true
+                    }
+                });
+                res.extend(avoid_colors);
+
+                // move low chroma colors to the end
+                let mut low_chroma = Vec::new();
+                res.retain(|c| {
+                    let lch = Lch::from_color(*c);
+                    if lch.chroma < 10. {
+                        low_chroma.push(c.clone());
+                        false
+                    } else {
+                        true
+                    }
+                });
+                res.extend(low_chroma);
+
+                if bg_config.save_kmeans {
+                    if let Ok(kmeans_config) = kmeans_config {
+                        if let Err(err) = kmeans_config.set(&kmeans_p, KmeanState(res.clone())) {
+                            tracing::error!("Failed to save the kmeans result: {}", err);
+                        }
+                    }
+                }
+
+                res
+            },
+        };
 
     let (builder_config, default) = if is_dark {
         (ThemeBuilder::dark_config()?, Theme::dark_default())
@@ -205,30 +256,54 @@ fn apply_state(
     // BG
     let default_window_bg = Lch::from_color(default.background.base);
 
-    let new_window_bg = res.remove(0);
-    centroids.retain(|c| c != &new_window_bg.into_color());
-    let mut new_window_bg: Lch = new_window_bg.into_color();
-    if (new_window_bg.chroma - default_window_bg.chroma).abs() > 15. {
-        new_window_bg.chroma = default_window_bg.chroma + 15.;
-        new_window_bg = new_window_bg.clamp();
-    }
-    new_window_bg.l = default_window_bg.l;
+    for c in &res {
+        // make sure not in avoid after adjusting
+        let mut new_window_bg: Lch = (*c).into_color();
+        if (new_window_bg.chroma - default_window_bg.chroma).abs() > 15. {
+            new_window_bg.chroma = default_window_bg.chroma + 15.;
+            new_window_bg = new_window_bg.clamp();
+        }
+        if is_dark
+            && bg_config.avoid_dark.iter().any(|a| {
+                let a = Lch::from_color(*a);
+                let hue_diff = (new_window_bg.hue.into_inner() - a.hue.into_inner()).abs() % 180.;
+                (new_window_bg.chroma - a.chroma).powf(2.) + (hue_diff).powf(2.) < 666.
+                    && hue_diff < 20.
+            })
+        {
+            continue;
+        } else if !is_dark
+            && bg_config.avoid_light.iter().any(|a| {
+                let a = Lch::from_color(*a);
+                let hue_diff = (new_window_bg.hue.into_inner() - a.hue.into_inner()).abs() % 180.;
+                (new_window_bg.chroma - a.chroma).powf(2.) + (hue_diff).powf(2.) < 666.
+                    && hue_diff < 20.
+            })
+        {
+            continue;
+        }
 
-    t = t.bg_color(new_window_bg.into_color());
+        new_window_bg.l = default_window_bg.l;
+
+        t = t.bg_color(new_window_bg.into_color());
+
+        res.retain(|c| {
+            let c = Lch::from_color(*c);
+            (c.hue - new_window_bg.hue).into_inner().abs() > 10.
+        });
+        break;
+    }
 
     // ACCENT
-    let clay_brown: Lch = Srgb::new(0.54, 0.38, 0.28).into_color();
-    let muddy_brown: Lch = Srgb::new(0.47, 0.34, 0.14).into_color();
-    let brown: Lch = Srgb::new(0.56078, 0.40784, 0.17647).into_color();
-    let brown2: Lch = Srgb::new(0.56078, 0.40784, 0.07).into_color();
-    let dark_gold: Lch = Srgb::new(0.651, 0.486, 0.443).into_color();
-    let gross_yellow: Lch = Srgb::new(0.439, 0.431, 0.078).into_color();
-    let gross_green: Lch = Srgb::new(0.47, 0.51, 0.32).into_color();
-    let avoid = vec![clay_brown, muddy_brown, dark_gold, brown, brown2, gross_yellow, gross_green];
+    let avoid =
+        if is_dark { &bg_config.avoid_accents_dark } else { &bg_config.avoid_accents_light };
 
-    let mut accent: (Lab, Lch) = (centroids[0], centroids[0].into_color());
+    let accent_res =
+        if bg_config.randomize { left_skewed_shuffle(res.clone(), Some(3)) } else { res.clone() };
+
+    let mut accent: (Lab, Lch) = (accent_res[0], accent_res[0].into_color());
     let mut best = f32::MIN;
-    for color in &res {
+    for (i, color) in accent_res.iter().enumerate() {
         let lch_orig = Lch::from_color(*color);
         let adjusted = adjust_lightness_for_contrast(
             (*color).into_color(),
@@ -237,11 +312,12 @@ fn apply_state(
         );
         let mut score = adjusted.chroma;
         if avoid.iter().any(|c| {
+            let c = Lch::from_color(*c);
             let hue_diff = (adjusted.hue.into_inner() - c.hue.into_inner()).abs() % 180.;
             (adjusted.chroma - c.chroma).powf(2.) + (hue_diff).powf(2.) < 666. && hue_diff < 20.
         }) {
             score /= 10.;
-        } else if lch_orig.chroma > 60. {
+        } else if lch_orig.chroma > 60. && i <= res.len() / 3 {
             accent = (*color, adjusted);
             break;
         }
@@ -250,7 +326,7 @@ fn apply_state(
             accent = (*color, adjusted);
         }
     }
-    let max_hue_diff = centroids
+    let max_hue_diff = res
         .iter()
         .map(|c| {
             let c = Lch::from_color(*c);
@@ -258,10 +334,7 @@ fn apply_state(
         })
         .max_by(|a, b| a.total_cmp(b))
         .unwrap();
-    centroids.retain(|c| {
-        let c = Lch::from_color(*c);
-        (c.hue - accent.1.hue).into_inner().abs() > max_hue_diff / 6.
-    });
+
     res.retain(|c| {
         let c = Lch::from_color(*c);
         (c.hue - accent.1.hue).into_inner().abs() > max_hue_diff / 6.
@@ -269,6 +342,8 @@ fn apply_state(
 
     let accent = Srgb::from_color(accent.1);
     t = t.accent(accent);
+
+    let mut res = if bg_config.randomize { left_skewed_shuffle(res, None) } else { res };
 
     // NEUTRAL
     let mut neutral = default.palette.neutral_5;
@@ -293,10 +368,77 @@ fn apply_state(
         neutral: t.neutral_tint.unwrap(),
         text: Some(text.into_color()),
     };
-    let my_config = cosmic_config::Config::new_state(ID, 1)?;
-    if let Err(err) = my_config.set(&p, result) {
-        tracing::error!("Failed to save the result: {}", err);
+    if bg_config.save_results {
+        let my_config = cosmic_config::Config::new_state(ID, 1)?;
+        if let Err(err) = my_config.set(&p, result) {
+            tracing::error!("Failed to save the result: {}", err);
+        }
     }
+
+    // PALETTE
+    // match chroma and lightness to accent for all palette colors
+    let blue = t.palette.as_mut().accent_blue;
+    t.palette.as_mut().accent_blue = sync_chroma_lightness(accent, blue);
+
+    let green = t.palette.as_mut().accent_green;
+    t.palette.as_mut().accent_green = sync_chroma_lightness(accent, green);
+
+    let orange = t.palette.as_mut().accent_orange;
+    t.palette.as_mut().accent_orange = sync_chroma_lightness(accent, orange);
+
+    let purple = t.palette.as_mut().accent_purple;
+    t.palette.as_mut().accent_purple = sync_chroma_lightness(accent, purple);
+
+    let red = t.palette.as_mut().accent_red;
+    t.palette.as_mut().accent_red = sync_chroma_lightness(accent, red);
+
+    let yellow = t.palette.as_mut().accent_yellow;
+    t.palette.as_mut().accent_yellow = sync_chroma_lightness(accent, yellow);
+
+    let ext_blue = t.palette.as_mut().ext_blue;
+    t.palette.as_mut().ext_blue = sync_chroma_lightness(accent, ext_blue);
+
+    let ext_indigo = t.palette.as_mut().ext_indigo;
+    t.palette.as_mut().ext_indigo = sync_chroma_lightness(accent, ext_indigo);
+
+    let ext_orange = t.palette.as_mut().ext_orange;
+    t.palette.as_mut().ext_orange = sync_chroma_lightness(accent, ext_orange);
+
+    let ext_pink = t.palette.as_mut().ext_pink;
+    t.palette.as_mut().ext_pink = sync_chroma_lightness(accent, ext_pink);
+
+    let ext_purple = t.palette.as_mut().ext_purple;
+    t.palette.as_mut().ext_purple = sync_chroma_lightness(accent, ext_purple);
+
+    let ext_warm_grey = t.palette.as_mut().ext_warm_grey;
+    t.palette.as_mut().ext_warm_grey = sync_chroma_lightness(accent, ext_warm_grey);
+
+    let ext_yellow = t.palette.as_mut().ext_yellow;
+    t.palette.as_mut().ext_yellow = sync_chroma_lightness(accent, ext_yellow);
+
+    let bright_green = t.palette.as_mut().bright_green;
+    t.palette.as_mut().bright_green =
+        Lch::from_color(sync_chroma_lightness(accent, bright_green)).saturate(0.5).into_color();
+
+    let bright_orange = t.palette.as_mut().bright_orange;
+    t.palette.as_mut().bright_orange =
+        Lch::from_color(sync_chroma_lightness(accent, bright_orange)).saturate(0.5).into_color();
+
+    let bright_red = t.palette.as_mut().bright_red;
+    t.palette.as_mut().bright_red =
+        Lch::from_color(sync_chroma_lightness(accent, bright_red)).saturate(0.5).into_color();
+
+    let accent_indigo = t.palette.as_mut().accent_indigo;
+    t.palette.as_mut().accent_indigo = sync_chroma_lightness(accent, accent_indigo);
+
+    let accent_pink = t.palette.as_mut().accent_pink;
+    t.palette.as_mut().accent_pink = sync_chroma_lightness(accent, accent_pink);
+
+    let accent_warm_grey = t.palette.as_mut().accent_warm_grey;
+    t.palette.as_mut().accent_warm_grey = sync_chroma_lightness(accent, accent_warm_grey);
+
+    let accent_yellow = t.palette.as_mut().accent_yellow;
+    t.palette.as_mut().accent_yellow = sync_chroma_lightness(accent, accent_yellow);
 
     t.write_entry(&builder_config)?;
 
@@ -306,7 +448,15 @@ fn apply_state(
 
     theme.write_entry(&theme_config)?;
 
-    Ok((centroids, res_ret))
+    Ok(())
+}
+
+fn sync_chroma_lightness(target: impl IntoColor<Lch>, c: impl IntoColor<Lch>) -> Srgba {
+    let target = target.into_color();
+    let mut c = c.into_color();
+    c.chroma = target.chroma;
+    c.l = target.l;
+    c.clamp().into_color()
 }
 
 // binary search modifying a's lightness to satisfy contrast with b
@@ -380,10 +530,71 @@ fn use_saved_result(path: &str, is_dark: bool) -> anyhow::Result<()> {
     Ok(())
 }
 
+// TODO add palette colors
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub struct BgResult {
     pub accent: Srgb,
     pub bg: Srgba,
     pub neutral: Srgb,
     pub text: Option<Srgb>,
+}
+
+/// Sorted colors
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct KmeanState(pub Vec<Lab>);
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CosmicConfigEntry)]
+#[version = 1]
+pub struct MyConfig {
+    pub avoid_accents_light: Vec<Srgb>,
+    pub avoid_accents_dark: Vec<Srgb>,
+    pub avoid_light: Vec<Srgb>,
+    pub avoid_dark: Vec<Srgb>,
+    pub save_results: bool,
+    pub save_kmeans: bool,
+    pub randomize: bool,
+}
+
+impl Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            avoid_accents_light: vec![
+                Srgb::new(0.54, 0.38, 0.28),
+                Srgb::new(0.47, 0.34, 0.14),
+                Srgb::new(0.56078, 0.40784, 0.17647),
+                Srgb::new(0.56078, 0.40784, 0.07),
+                Srgb::new(0.651, 0.486, 0.443),
+                Srgb::new(0.439, 0.431, 0.078),
+                Srgb::new(0.47, 0.51, 0.32),
+            ],
+            avoid_accents_dark: vec![
+                Srgb::new(0.54, 0.38, 0.28),
+                Srgb::new(0.47, 0.34, 0.14),
+                Srgb::new(0.56078, 0.40784, 0.17647),
+                Srgb::new(0.56078, 0.40784, 0.07),
+                Srgb::new(0.651, 0.486, 0.443),
+                Srgb::new(0.439, 0.431, 0.078),
+                Srgb::new(0.47, 0.51, 0.32),
+            ],
+            avoid_light: Vec::new(),
+            avoid_dark: vec![
+                Srgb::new(0.169, 0.165, 0.004),
+                Srgb::new(0.169, 0.098, 0.004),
+                Srgb::new(0.29, 0.18, 0.129),
+                Srgb::new(0.29, 0.271, 0.129),
+            ],
+            save_results: false,
+            save_kmeans: true,
+            randomize: true,
+        }
+    }
+}
+
+fn left_skewed_shuffle<T>(mut v: Vec<T>, max_len_swap: Option<usize>) -> Vec<T> {
+    let mut rng = rand::thread_rng();
+    for i in 0..max_len_swap.unwrap_or_else(|| v.len()) {
+        let j = rng.gen_range(i..v.len());
+        v.swap(i, j);
+    }
+    v
 }
